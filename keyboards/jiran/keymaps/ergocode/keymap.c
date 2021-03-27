aee6c49aa8be3b139f05eaa4bc57141b7b452b0c
Sergey Malenkov <def.sergey@gmail.com>
2021-03-27 17:49:38 +0300

    WIP new layout inspired by rs40
    * keyboards/jiran/keymaps/ergocode/keymap.c:

--- keyboards/jiran/keymaps/ergocode/keymap.c
+++ keyboards/jiran/keymaps/ergocode/keymap.c
@@ -12,35 +12,36 @@ enum jiran_layers {
 #define KC_SLSF  RSFT_T(KC_LSCR)
 #define KC_NLCT  RCTL_T(KC_LNUM)
 #define KC_ETAL  RALT_T(KC_ENT)
-#define LOWER  MO(_LOWER) 
+#define LOWER  MO(_LOWER)
+#define SPACE LSFT_T(KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //          ┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-               KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+               KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                               KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
   // ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐
-      KC_LGUI, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBGI,
+      KC_NO,   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_NO,
   // └────────┼────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┼────────┘
-               KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QTSH,
+               KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QTSH,
   //          ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-               KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSCT,
+               KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
   //          └────────┴────────┴────────┴────────┼────────┼────────┤                          ├────────┼────────┼────────┴────────┴────────┴────────┘
-                                          LOWER,   KC_SPC,  KC_LALT,                            KC_ETAL, KC_BSPC, LOWER
+                                          LOWER,   KC_RCTL,  SPACE,                               SPACE,   KC_LALT, LOWER
                                   //     └────────┴────────┴────────┘                          └────────┴────────┴────────┘
   ),
 
   [_LOWER] = LAYOUT(
   //          ┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-               KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,
+               KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,
   // ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐
-      KC_F11,  KC_TAB,  RGB_HUI, KC_HOME, RESET,   RGB_SAI, RGB_VAI,                            KC_VOLU, KC_PGUP, RESET,   KC_HOME, KC_INS,  KC_DEL,  KC_F12,
+      KC_NO,  KC_ESC,  S(KC_1), S(KC_2), S(KC_3),  S(KC_4), S(KC_5),                            S(KC_6), S(KC_9), S(KC_8), S(KC_0), KC_MINUS, KC_EQUAL, KC_NO,
   // └────────┼────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┼────────┘
-               KC_LSFT, RGB_HUD, KC_LEFT, KC_UP,   KC_RGHT, RGB_VAD,                            KC_MUTE, KC_LEFT, KC_UP,   KC_RGHT, KC_PSCR, KC_SLSF,
+               KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               S(KC_7), KC_LBRC, KC_UP,   KC_RBRC, KC_TRNS, KC_BSLS,
   //          ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-               KC_LCTL, RGB_M_B, KC_END,  KC_DOWN, RGB_SAD, RGB_TOG,                            KC_VOLD, KC_PGDN, KC_DOWN, KC_END,  KC_PAUS, KC_NLCT,
+               KC_CAPS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                               KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_ENT,
   //          └────────┴────────┴────────┴────────┼────────┼────────┤                          ├────────┼────────┴────────┴────────┴────────┴────────┘
-                                          KC_TRNS, KC_SPC,  KC_LALT,                            KC_ETAL, KC_BSPC, KC_TRNS
+                                          KC_TRNS, KC_RCTL,  SPACE,                               SPACE, KC_LALT, KC_TRNS
                                     //   └────────┴────────┴────────┘                          └────────┴────────┴────────┘
   ),
 
